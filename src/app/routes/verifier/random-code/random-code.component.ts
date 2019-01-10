a420eeafd66d205dbe63f4899fb0c70a80934ee7
chenl <chenl@caiyoudata.com>
2019-01-10 15:07:56 +0800

    random-code

--- src/app/routes/verifier/random-code/random-code.component.ts
+++ src/app/routes/verifier/random-code/random-code.component.ts
@@ -25,7 +25,7 @@ export class VerifierRandomCodeComponent implements OnInit {
     const ctx = canvas.getContext('2d');
     // 随机内容
     const sCode =
-      'a,b,c,d,e,f,g,h,i,j,k,m,n,p,q,r,s,t,u,v,w,x,y,z,A,B,C,E,F,G,H,J,K,L,M,N,P,Q,R,S,T,W,X,Y,Z,1,2,3,4,5,6,7,8,9,0';
+      'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,1,2,3,4,5,6,7,8,9,0';
     const aCode = sCode.split(',');
     // 获取到数组长度
     const aLength = aCode.length;
